425ad0710a547ae37a4a2f89022a6ca977ecbeae
LRharlekin <13718192+LRharlekin@users.noreply.github.com>
2024-03-13 13:00:30 +0200

    l9

---
+++ components/ProductDetails.js
@@ -0,0 +1,20 @@
+app.component("product-details", {
+  props: {
+    details: {
+      type: Array,
+      required: true,
+    },
+  },
+  template:
+    /* html */
+    `<div class="product-details">
+        <ul>
+          <li v-for="detail in details">{{ detail }}</li>
+        </ul>
+      </div>`,
+  data() {
+    return {};
+  },
+  methods: {},
+  computed: {},
+});
